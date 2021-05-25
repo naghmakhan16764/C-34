be9795ef98241e869bb8804c21d2d05cb49402b1
naghmakhan16764 <74590340+naghmakhan16764@users.noreply.github.com>
2021-05-25 20:33:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 var bg, ground, gimg;
-var blocks=[]
+var blocks = [];
+var chain;
 
 function setup() {
   createCanvas(1300, 600);
@@ -12,11 +13,13 @@ function setup() {
   world = engine.world;
 
   ground = new Ground();
-  ball = new Ball(300,300);
-  for (var i = 700 ; i<900;i=i+21 ){
-      for (var j = 300;j<500; j=j+30){
-          blocks.push(new Block (i,j))
-      }
+  ball = new Ball(300, 300);
+  chain = new SlingShot(ball.body, { x: 200, y: 200 });
+
+  for (var i = 700; i < 900; i = i + 21) {
+    for (var j = 300; j < 500; j = j + 30) {
+      blocks.push(new Block(i, j));
+    }
   }
 }
 function draw() {
@@ -25,10 +28,17 @@ function draw() {
 
   ground.display();
   ball.display();
-  
-  
-  for (var j = 0;j<blocks.length; j=j+1){
-    blocks[j].display()
+  chain.display();
+
+  for (var j = 0; j < blocks.length; j = j + 1) {
+    blocks[j].display();
+  }
 }
 
+function mouseDragged() {
+  Matter.Body.setPosition(ball.body, { x: mouseX, y: mouseY });
+}
+
+function mouseReleased() {
+  chain.fly();
 }
